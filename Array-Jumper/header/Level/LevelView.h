7c15f5e3335fefa27e601e0227ec9cfae066ea5d
bhusanSahoo <bhusan12sahoo@gmail.com>
2025-02-11 16:22:44 +0530

    .

--- Array-Jumper/header/Level/LevelView.h
+++ Array-Jumper/header/Level/LevelView.h
@@ -14,21 +14,35 @@ namespace Level
 		LevelController* level_controller;
 		
 		 
-
+		//background 
 		UI::UIElement::ImageView* background_image;
 		const float background_alpha = 110.f;
 
+
+		//boxes
 		BoxDimensions box_dimensions;
 		UI::UIElement::ImageView* box_image;
 
+		//overlay image
+		UI::UIElement::ImageView* target_overlay_image;
+		UI::UIElement::ImageView* letter_one_overlay_image;
+		UI::UIElement::ImageView* letter_two_overlay_image;
+		UI::UIElement::ImageView* letter_three_overlay_image;
+		UI::UIElement::ImageView* obstacle_one_overlay_image;
+		UI::UIElement::ImageView* obstacle_two_overlay_image;
 
-
+		
 		void createImages();
 		void initializeImages();
 		void updateImages();
 		void drawLevel();
 		void deleteImages();
 		void calculateBoxDimensions();
+
+		ImageView* getBoxOverlayImage(BlockType block_type);
+
+		void drawBox(sf::Vector2f position);
+		void drawBoxValue(sf::Vector2f position, BlockType box_value);
 
 	public:
 		LevelView(LevelController* controller);
